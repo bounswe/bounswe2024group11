8344d8bb4cc7de6c097698837f2c2e1bd46dd2b6
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-04-20 09:52:07 +0300

    chore(web): cleanup more stuff

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -1,5 +1,8 @@
 import { createTheme } from "@mantine/core";
+import { DEFAULT_THEME } from "@mantine/core";
 
-export const theme = createTheme({
-  /* Put your mantine theme override here */
-});
+export const theme = DEFAULT_THEME;
+
+// export const theme = createTheme({
+//   /* Put your mantine theme override here */
+// });
