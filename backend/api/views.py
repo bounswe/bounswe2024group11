c6746b517eca23f8d6da02d0e03d84210573b786
Memin Seeker <72305188+Meminseeker@users.noreply.github.com>
2024-04-20 21:32:15 +0300

    feat: Add User 'GET' view

---
+++ backend/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from user.models import User
+from .serializer import UserSerializer
+
+@api_view(['GET'])
+def getUsers(request):
+    items = User.objects.all()
+    serializer = UserSerializer(items, many=True)
+    return Response(serializer.data)
