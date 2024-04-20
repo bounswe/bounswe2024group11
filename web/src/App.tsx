bb68b702e20b49f07e59a06ad09acd03acd5fec0
hks1444 <95296467+hks1444@users.noreply.github.com>
2024-04-20 12:57:18 +0300

    feat(web): create example routes for csr (#189)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,101 @@
-import { Container, MantineProvider } from "@mantine/core";
+import { Container, MantineProvider, Button, Flex } from "@mantine/core";
 import "@mantine/core/styles.css";
 import React from "react";
 import { theme } from "./theme";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import img from "../public/zenith-logo.svg";
 
+type NumberButtonProps = {
+  digit: number,
+}
+
+const Buttons = ({ digit }: NumberButtonProps) => {
+  return (
+    <Flex
+      mih={50}
+      gap="md"
+      justify="center"
+      align="flex-start"
+      direction="column"
+      wrap="wrap">
+      {(digit != -1 && <Button
+        component="a"
+        href="/"
+      >
+        Home
+      </Button>)}
+      {(digit != 0 && <Button
+        component="a"
+        href="/userprofile/1"
+      >
+        User Profile for User 1
+      </Button>)}
+      {(digit != 1 && <Button
+        component="a"
+        href="/register"
+      >
+        Register
+      </Button>)}
+      {(digit != 2 && <Button
+        component="a"
+        href="/login"
+      >
+        Login
+      </Button>)}
+      {(digit != 3 && <Button
+        component="a"
+        href="/feed"
+      >
+        Feed
+      </Button>)}
+    </Flex>
+  )
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:
+      <Container>
+        <Outlet />
+        <Buttons digit={-2}></Buttons>
+      </Container>,
+    children: [
+      {
+        path: "userprofile/:profileId",
+        element:
+          <Container>
+            <h1>User Profile</h1>
+          </Container>,
+      },
+      {
+        path: "login",
+        element:
+          <Container>
+            <h1>Login</h1>
+          </Container>,
+      },
+      {
+        path: "register",
+        element:
+          <Container>
+            <h1>Register</h1>
+          </Container>,
+      },
+      {
+        path: "feed",
+        element:
+          <Container>
+            <h1>Feed</h1>
+          </Container>,
+      },
+    ],
+  },
+]);
 export default function App() {
   return (
     <MantineProvider theme={theme}>
@@ -15,6 +108,7 @@ export default function App() {
           }}
         />
       </Container>
+      <RouterProvider router={router} />
     </MantineProvider>
   );
 }
