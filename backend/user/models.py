3d0b42fddb867b6a5df0b9137d37d9f3b4b63a51
Memin Seeker <72305188+Meminseeker@users.noreply.github.com>
2024-04-20 21:19:30 +0300

    feat: Define user model

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
