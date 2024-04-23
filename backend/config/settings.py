23674efac6d750a00a355ca9b2c51b33da0ef160
yunuskaydin <yunuskaganaydin@gmail.com>
2024-04-23 12:10:22 +0300

    feat: add dotenv to settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv('./.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
