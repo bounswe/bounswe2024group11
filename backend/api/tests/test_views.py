ffcbfcd4f5a1ebab6079310eb78a97fb16ffc4f8
MucahitErdoganUnlu <mucahit.719@hotmail.com>
2024-04-23 18:43:43 +0300

    test: add test_update_user

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -1,4 +1,6 @@
 from .test_setup import TestSetUp
+from django.urls import reverse
+from faker import Faker
 
 class TestView(TestSetUp):
     
@@ -22,3 +24,18 @@ class TestView(TestSetUp):
         self.assertEqual(response.data[-1]['username'], self.user_data['username'])
         self.assertEqual(response.data[-1]['name'], self.user_data['name'])
         self.assertEqual(response.data[-1]['surname'], self.user_data['surname'])
+
+    def test_update_user(self):
+        response = self.client.post(self.add_user_url, self.user_data, format='json')
+        self.update_user_url = reverse('update_user', kwargs={'pk': response.data['id']})
+        self.user_data['email'] = Faker().email()
+        self.user_data['username'] = Faker().user_name()
+        self.user_data['name'] = Faker().first_name()
+        self.user_data['surname'] = Faker().last_name()
+
+        response = self.client.put(self.update_user_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['username'], self.user_data['username'])
+        self.assertEqual(response.data['email'], self.user_data['email'])
+        self.assertEqual(response.data['name'], self.user_data['name'])
+        self.assertEqual(response.data['surname'], self.user_data['surname'])
