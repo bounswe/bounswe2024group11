03566697bc67eb2ee52bad10bf9d6a53e7f6734e
Yunus Kağan Aydın <133023181+yunuskaydin@users.noreply.github.com>
2024-04-23 12:50:11 +0300

    Connect Backend to Hosted MySQL Server (#221)
    Co-authored-by: yunuskaydin <yunuskaganaydin@gmail.com>

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv('./.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +81,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),   # Or your MySQL server's IP address
+        'PORT': os.getenv('PORT'),        # Default MySQL port
     }
 }
+
 
 
 # Password validation
