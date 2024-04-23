0ebfb5a22cf90608a2fe80dfc9358dd66a7e128f
MucahitErdoganUnlu <mucahit.719@hotmail.com>
2024-04-23 18:30:16 +0300

    feat: Update test setup in test_setup.py to use Faker for generating test data

--- backend/api/tests/test_setup.py
+++ backend/api/tests/test_setup.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from faker import Faker
 
 class TestSetUp(APITestCase):
     def setUp(self):
@@ -9,10 +10,10 @@ class TestSetUp(APITestCase):
         self.delete_user_url = reverse('delete_user', kwargs={'pk': 1})
 
         self.user_data = {
-            'email': 'email@gmail.com',
-            'username': 'username',
-            'name': 'name',
-            "surname": "surname",
+            'email': Faker().email(),
+            'username': Faker().user_name(),
+            'name': Faker().first_name(),
+            "surname": Faker().last_name(),
         }
 
         return super().setUp()
