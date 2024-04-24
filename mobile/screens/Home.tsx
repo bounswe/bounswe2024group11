1393f91194489a37ef83b6d011a1d61245b1f18f
tenacke <emrekilic1010@gmail.com>
2024-04-24 21:40:19 +0300

    feat(mobile): added screens

---
+++ mobile/screens/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native-ui-lib";
+
+const Home = () => {
+    return (
+        <View flex center>
+            <Text>Home</Text>
+        </View>
+    )
+};
+
+export default Home;
