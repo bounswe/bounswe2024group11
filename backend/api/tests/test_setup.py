7dca843d960fb3b99294ac55efa89f2975a239f8
Mücahit Erdoğan Ünlü <94074126+MucahitErdoganUnlu@users.noreply.github.com>
2024-04-24 12:30:28 +0300

    test(api): add unit tests for user model (#224)

---
+++ backend/api/tests/test_setup.py
@@ -0,0 +1,22 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from faker import Faker
+
+class TestSetUp(APITestCase):
+    def setUp(self):
+        self.get_users_url = reverse('get_users')
+        self.add_user_url = reverse('add_user')
+        self.update_user_url = reverse('update_user', kwargs={'pk': 1})
+        self.delete_user_url = reverse('delete_user', kwargs={'pk': 1})
+
+        self.user_data = {
+            'username': Faker().user_name(),
+            'email': Faker().email(),
+            'name': Faker().first_name(),
+            "surname": Faker().last_name(),
+        }
+
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()   
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getUsers),
-    path('add/', views.addUser),
-    path('update/<str:pk>/', views.updateUser),
-    path('delete/<str:pk>/', views.deleteUser),
+    path('', views.getUsers, name="get_users"),
+    path('add/', views.addUser, name="add_user"),
+    path('update/<str:pk>/', views.updateUser, name="update_user"),
+    path('delete/<str:pk>/', views.deleteUser, name="delete_user"),
 ]
