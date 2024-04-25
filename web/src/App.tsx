80cbd2d5a280fd36c4bcd1e1ce62f16299c53429
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-04-25 16:34:56 +0300

    fix(web): work on router and login page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,82 +1,19 @@
 import { Container, MantineProvider, Button, Flex, Input } from "@mantine/core";
 import "@mantine/core/styles.css";
 import React from "react";
-import { PokeLoader, href } from "./router";
-import { theme } from "./theme";
 import * as ReactDOM from "react-dom/client";
 import { Link, Form, useSubmit } from "react-router-dom";
 import { useLoaderData } from "react-router-typesafe";
 import img from "../public/zenith-logo.svg";
-
-type NumberButtonProps = {
-  digit: number;
-};
-
-const Buttons = ({ digit }: NumberButtonProps) => {
-  return (
-    <Flex
-      mih={50}
-      gap="md"
-      justify="center"
-      align="flex-start"
-      direction="column"
-      wrap="wrap"
-    >
-      {digit != -1 && (
-        <Button component="a" href="/">
-          Home
-        </Button>
-      )}
-      {digit != 0 && (
-        <Button component="a" href="/userprofile/1">
-          User Profile for User 1
-        </Button>
-      )}
-      {digit != 1 && (
-        <Button component="a" href="/register">
-          Register
-        </Button>
-      )}
-      {digit != 2 && (
-        <Button component="a" href="/login">
-          Login
-        </Button>
-      )}
-      {digit != 3 && (
-        <Button component="a" href="/feed">
-          Feed
-        </Button>
-      )}
-    </Flex>
-  );
-};
+import { href } from "./router";
 
 // https://pokeapi.co/api/v2/
 // pokemon/ditto
 export const App = () => {
-  const a = useLoaderData<PokeLoader>();
   const submit = useSubmit();
   return (
-    <MantineProvider theme={theme}>
-      <Link to={href({ path: "/type" })}>aaahaha</Link>
-      <Container>
-        <img src="./zenith-logo.svg" alt="Zenith Logo" />
-        <div
-          className="h-10 w-10 opacity-50"
-          style={{
-            background: theme.colors.cyan[5],
-          }}
-        />
-        <Form
-          onChange={(e) => {
-            submit(e.currentTarget);
-          }}
-        >
-          <input name="search" placeholder="Enter your name" />
-          <button>Submit</button>
-        </Form>
-        <p>{JSON.stringify(a.pokemon)}</p>
-      </Container>
-    </MantineProvider>
+    <Container>
+      <Link to={href({ path: "/login" })}> Log In </Link>
+    </Container>
   );
 };
--- web/src/Login.tsx
+++ web/src/Login.tsx
@@ -1,43 +1,63 @@
-import { TextInput, Container, MantineProvider, Button, Flex, Input, Checkbox } from "@mantine/core";
+import {
+  TextInput,
+  Container,
+  Button,
+  Flex,
+  Input,
+  Checkbox,
+} from "@mantine/core";
 import { Link, Form, useSubmit } from "react-router-dom";
-import { useForm } from '@mantine/form';
-import { useState } from 'react';
-const submit = useSubmit();
+import { href } from "./router";
 
 export const Login = () => {
-    return (
-        <Flex
-            mih={50}
-            gap="md"
-            justify="center"
-            align="center"
-            direction="column"
-            wrap="wrap-reverse"
+  const submit = useSubmit();
+  return (
+    <Container>
+      <div className="flex flex-col items-center justify-center min-h-12 gap-4">
+        <div className="flex flex-col">
+          <Flex justify="center" align="flex-start" direction="column">
+            <img src="./zenith-logo.svg" alt="Zenith Logo" />
+          </Flex>
+          <h2 text-align="center">
+            <b>Login to your Zenith account</b>
+          </h2>
+          <p text-align="center">Ready to continue your comic adventure?</p>
+        </div>
+        <Form
+          onChange={(e) => {
+            submit(e.currentTarget);
+          }}
         >
-            <Flex>
-                <Flex
-                    justify="center"
-                    align="flex-start"
-                    direction="row">
-                    <img src="./zenith-logo.svg" alt="Zenith Logo" />
-                </Flex>
-                <h2 text-align="center"><b>Login to your Zenith account</b></h2>
-                <p text-align="center">Ready to continue your comic adventure?<br />Login now!</p>
-            </Flex>
+          <input name="login" placeholder="email address" id="Email" required />
+          <input
+            name="password"
+            type="password"
+            placeholder="password"
+            required
+          />
+          <Container>
+            <input type="checkbox" name="Keep me logged in" />
+            <Link to="/">Forgot Password</Link>
+          </Container>
 
-            <Form
-                onChange={(e) => {
-                    submit(e.currentTarget);
-                }}
-            >
-                <input name="login" placeholder="email address" id="Email" required />
-                <input name="password" type="password" placeholder="password" required />
-                <Container>
-                    <input type="checkbox" name="Keep me logged in" />
-                    <Link to="/">Forgot Password</Link>
-                </Container>
-                <Button type="submit" fullWidth>Login</Button>
-            </Form>
-        </Flex>
-    );
-}
+          <Button
+            to={href({ path: "/" })}
+            component={Link}
+            type="submit"
+            fullWidth
+          >
+            Log Innn
+          </Button>
+        </Form>
+        <Button
+          to={href({ path: "/register" })}
+          component={Link}
+          type="submit"
+          fullWidth
+        >
+          Register
+        </Button>
+      </div>
+    </Container>
+  );
+};
