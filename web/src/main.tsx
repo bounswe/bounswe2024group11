588e4336a86ac81d1f6fe126a219a8e3c67265b3
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-04-25 18:41:40 +0300

    style(web): add design tokens and style configs

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <MantineProvider classNamesPrefix="znt" theme={theme}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </MantineProvider>
 );
