60e9a9b8cd7d1a2d3b5b535a3e84a1af0c463cc4
Ümit Can Evleksiz <68506701+umitcan07@users.noreply.github.com>
2024-04-26 17:54:08 +0300

    feat(mobile): add home screen navigation (#243)
    Co-authored-by: tenacke <emrekilic1010@gmail.com>

--- mobile/screens/Home.tsx
+++ mobile/screens/Home.tsx
@@ -1,13 +1,79 @@
 import React from "react";
 
-import { View, Text } from "react-native-ui-lib";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const Home = () => {
-    return (
-        <View flex center>
-            <Text>Home</Text>
-        </View>
-    )
-};
+import Feed from "./Feed";
+import Search from "./Search";
+import Profile from "./Profile";
+import { styles } from "../components/Styles";
+
+const Tab = createMaterialBottomTabNavigator();
+
+function Home() {
+  return <BottomTab />;
+}
+
+function BottomTab() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        labeled={false}
+        barStyle={styles.tabBar}
+        activeColor="black"
+        inactiveColor="gray"
+        shifting={true}
+      >
+        <Tab.Screen
+          name="Feed"
+          component={Feed}
+          options={{
+            tabBarIcon: ({ color, focused }) => (
+              <MaterialCommunityIcons
+                name="home"
+                color={color}
+                size={focused ? 28 : 24}
+                style={focused ? styles.icon : {}}
+              />
+            ),
+            tabBarColor: "white",
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="Search"
+          component={Search}
+          options={{
+            tabBarIcon: ({ color, focused }) => (
+              <MaterialCommunityIcons
+                name="magnify"
+                color={color}
+                size={focused ? 28 : 24}
+                style={focused ? styles.icon : {}}
+              />
+            ),
+            tabBarColor: "white",
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: ({ color, focused }) => (
+              <MaterialCommunityIcons
+                name="account"
+                color={color}
+                size={focused ? 28 : 24}
+                style={focused ? styles.icon : {}}
+              />
+            ),
+            tabBarColor: "white",
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default Home;
