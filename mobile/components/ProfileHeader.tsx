16d49ae2c65e2b7fb4e3ba5c68731398a1a2b38a
tenacke <emrekilic1010@gmail.com>
2024-04-27 01:24:36 +0300

    feat(mobile): added profile page

---
+++ mobile/components/ProfileHeader.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+import { View, Text, Image } from "react-native";
+import { Appbar, Button } from "react-native-paper";
+
+import { MaterialBottomTabNavigationProp } from "@react-navigation/material-bottom-tabs";
+
+import { RootStackParamList } from "./Types";
+import { styles } from "./Styles";
+import { useUser } from "../context/UserContext";
+
+type ProfileNavigationProp = MaterialBottomTabNavigationProp<
+  RootStackParamList,
+  "Profile"
+>;
+
+const ProfileHeader = ({
+  navigation,
+}: {
+  navigation: ProfileNavigationProp;
+}) => {
+  const { user, setUser } = useUser();
+  const handleLogOut = () => {
+    setUser({ isLogged: false });
+    navigation.navigate("Login");
+  };
+
+  return (
+    <Appbar.Header style={styles.appBar}>
+      <Appbar.Content
+        title={
+          <View style={styles.headerLogo}>
+            <Image
+              source={require("../assets/zenith-logo.png")}
+              style={styles.headerLogoImage}
+            />
+            <Text style={styles.headerText}>Zenith</Text>
+          </View>
+        }
+      />
+
+      <Appbar.Content
+        title={
+          <View style={styles.headerRight}>
+            {user.isLogged && (
+              <Button
+                mode="contained"
+                onPress={handleLogOut}
+                style={styles.headerButton}
+              >
+                Log Out
+              </Button>
+            )}
+          </View>
+        }
+      />
+    </Appbar.Header>
+  );
+};
+
+export default ProfileHeader;
