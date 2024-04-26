56bde1ebe231f5ce7daabdf5bdee30313dda3b34
CEYDANUR ŞEN <ceydasen40@gmail.com>
2024-04-26 15:13:29 +0300

    Add password field to User model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,31 +1,44 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from user.models import User
 from .serializer import UserSerializer
+from rest_framework import permissions, status
+
 
 @api_view(['GET'])
+
 def getUsers(request):
     items = User.objects.all()
     serializer = UserSerializer(items, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
+
 def addUser(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT'])
 def updateUser(request, pk):
-    item = User.objects.get(id=pk)
+    try:
+        item = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
     serializer = UserSerializer(instance=item, data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
+
 def deleteUser(request, pk):
-    item = User.objects.get(id=pk)
+    try:
+        item = User.objects.get(id=pk)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
     item.delete()
-    return Response({"res":"Object deleted!"})
+    return Response({"res":"Object deleted!"}, status=status.HTTP_204_NO_CONTENT)
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.hashers import make_password, check_password
 
 class User(models.Model):
     username = models.CharField(max_length=100)
@@ -8,3 +7,10 @@ class User(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=128)  # Updated to allow for hashed passwords
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
