61f22775977c870472d32996651aff1f9a69849f
Mücahit Erdoğan Ünlü <94074126+MucahitErdoganUnlu@users.noreply.github.com>
2024-04-27 22:40:57 +0300

    feat(backend): send proper respond status codes (#279)
    Co-authored-by: Yunus Kağan Aydın <133023181+yunuskaydin@users.noreply.github.com>

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,6 +8,10 @@ from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 def register(request):
+    required_fields = ['username', 'password', 'email']
+    if not all([field in request.data for field in required_fields]):
+        return Response({"res":"Please provide all required fields."}, status=status.HTTP_400_BAD_REQUEST)
+    
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -15,20 +19,27 @@ def register(request):
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        return Response({"token": token.key, "user": serializer.data})
+        return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def login(request):
-    user = get_object_or_404(User, username=request.data['username'])
+    required_fields = ['username', 'password']
+    if not all([field in request.data for field in required_fields]):
+        return Response({"res":"Please provide both username and password."}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        user = User.objects.get(username=request.data['username'])
+    except User.DoesNotExist:
+        return Response({"res": "Username not found."}, status=status.HTTP_401_UNAUTHORIZED)
+
     if not user.check_password(request.data['password']):
-        return Response({"res":"password does not match."}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"res":"password does not match."}, status=status.HTTP_401_UNAUTHORIZED)
     token, created = Token.objects.get_or_create(user=user)
     serializer = UserSerializer(instance=user)
-    return Response({"token": token.key, "user": serializer.data})
+    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 
 #@api_view(['GET'])
 #def test_token(request):
 #    return Response({"res":"Token is valid!"})
-
