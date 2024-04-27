b32924ef67595972b10ec80e98b37aca145f3743
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-04-27 22:57:20 +0300

    chore(web): remove terms comment

--- web/src/Terms.tsx
+++ web/src/Terms.tsx
@@ -2,20 +2,6 @@ import { Container } from "@mantine/core";
 import { href } from "./router";
 import { button, buttonInnerRing } from "./components/Button";
 
-// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-// Version 2, December 2004
-
-// Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
-
-// Everyone is permitted to copy and distribute verbatim or modified
-// copies of this license document, and changing it is allowed as long
-// as the name is changed.
-
-// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-// TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
-// 0. You just DO WHAT THE FUCK YOU WANT TO.
-
 export const Terms = () => {
   return (
     <Container className="py-20">
