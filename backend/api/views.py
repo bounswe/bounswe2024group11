54d37c106127f6fbd50c13fd8d0b7f1b2e05447b
MucahitErdoganUnlu <mucahit.719@hotmail.com>
2024-04-27 16:29:25 +0300

    fix(backend): add correct status codes for login and signup

--- backend/api/views.py
+++ backend/api/views.py
@@ -19,7 +19,7 @@ def register(request):
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        return Response({"token": token.key, "user": serializer.data})
+        return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
@@ -28,12 +28,16 @@ def login(request):
     if not all([field in request.data for field in required_fields]):
         return Response({"res":"Please provide both username and password."}, status=status.HTTP_400_BAD_REQUEST)
 
-    user = get_object_or_404(User, username=request.data['username'])
+    try:
+        user = User.objects.get(username=request.data['username'])
+    except User.DoesNotExist:
+        return Response({"res": "Username not found."}, status=status.HTTP_401_UNAUTHORIZED)
+
     if not user.check_password(request.data['password']):
-        return Response({"res":"password does not match."}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"res":"password does not match."}, status=status.HTTP_401_UNAUTHORIZED)
     token, created = Token.objects.get_or_create(user=user)
     serializer = UserSerializer(instance=user)
-    return Response({"token": token.key, "user": serializer.data})
+    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 
 #@api_view(['GET'])
