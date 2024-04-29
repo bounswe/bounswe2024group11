8ff4fd6ff492b025a76eed0a838c496c00c1c358
Ceydanur Şen <ceydasen40@gmail.com>
2024-04-29 16:19:25 +0300

    feature(backend): added place of birth semantic search feature

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.authtoken.models import Token
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+import requests
 
 @swagger_auto_schema(
     method='post',
@@ -85,3 +86,101 @@ def login(request):
 #@api_view(['GET'])
 #def test_token(request):
 #    return Response({"res":"Token is valid!"})
+my_dict = {
+    "born in new york": "Q60",
+    "born in new jersey": "Q1408",
+    "born in themyscira":"Q2809472",
+    "born in iowa":"Q1546"
+  
+}
+
+@api_view(["GET"])
+def search(request):
+    #this part should be changed because we should use post to get keyword.
+    if request.method == 'GET':
+        #keyword = request.POST.get(request.data.get("keyword"), '')
+        keyword = request.data["keyword"].lower()
+        print(my_dict[keyword])
+        print(keyword)
+        # Construct SPARQL query
+        qid = my_dict[keyword]
+    
+
+        birtOfPlace_query = """
+        #New York doğumlu insanlar
+        #title: Humans born in New York City
+        SELECT DISTINCT ?item ?itemLabel ?itemDescription ?sitelinks ?placeOfBirthLabel 
+        WHERE {
+            ?item wdt:P31 wd:Q1114461;            # Any instance of a human
+                  wdt:P19/wdt:P131* wd:%s;
+                 
+                  wikibase:sitelinks ?sitelinks.
+                  OPTIONAL{
+                   ?item wdt:P19 ?placeOfBirth.
+                   ?placeOfBirth rdfs:label ?placeOfBirthLabel.
+                   FILTER(LANG(?placeOfBirthLabel) ="en").
+                  }
+                  
+                    # Who was born in any value (eg. a hospital)
+        # that has the property of 'administrative area of' New York City or New York City itself.
+
+        # Note that using wdt:P19 wd:Q60;  # Who was born in New York City.
+        # Doesn't include humans with the birth place listed as a hospital
+        # or an administrative area or other location of New York City.
+
+                
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+        }
+        ORDER BY DESC(?sitelinks)
+        """ %qid
+
+        print(my_dict[keyword])
+        # Construct SPARQL query for comic characters
+        
+
+        # Parameters for the API call
+        birtOfPlace_query_params = {
+            'query': birtOfPlace_query,
+            'format': 'json'  # Response format
+        }
+        
+        
+
+        # API endpoint
+        url = 'https://query.wikidata.org/sparql'
+
+        # Make the API call
+        birtOfPlace_response = requests.get(url, params=birtOfPlace_query_params)
+        #character_response = requests.get(url, params=character_params)
+        
+        # Check if the request was successful
+        if birtOfPlace_response.status_code == 200 :
+            print("ceyda")
+            # Parse JSON response
+            birth_data = birtOfPlace_response.json()
+            #character_data = character_response.json()
+
+            #print(comic_response.json())
+            #print(character_response.json())
+           
+
+            birth_of_results = [{'type': 'character', 
+                              'label': item['itemLabel']['value'], 
+                              'description': item['itemDescription']['value'],
+                              'place': item.get('placeOfBirthLabel',{}).get('value','Unknown'),
+                              'siteLinks': item.get('sitelinks',{}).get('value','Unknown'),
+
+
+                             } for item in birth_data['results']['bindings']]
+          
+
+            combined_results = birth_of_results
+            
+            print(combined_results)
+            
+            
+            return Response({'keyword': keyword, 'results': combined_results})
+        else:
+            return Response({'error': 'Failed to retrieve data from Wikidata.'}, status=500)
+    else:
+        return Response({'error': 'Invalid request method. Only GET method is allowed.'}, status=405)
