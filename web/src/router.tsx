795d1a578180b9047acc345ade303404965b2902
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-04-29 15:29:32 +0300

    feat(web): add home page search action

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -3,16 +3,16 @@ import { Home } from "./routes/Home";
 import { Login } from "./routes/Login";
 import { Register } from "./routes/Register";
 import { Terms } from "./routes/Terms";
-import { homeLoader } from "./routes/Home.data";
+import { homeAction, homeLoader } from "./routes/Home.data";
 import { loginAction, loginLoader } from "./routes/Login.data";
 import { registerAction, registerLoader } from "./routes/Register.data";
-import { Feed } from "./routes/HomePage";
 
 export const { router, href } = typesafeBrowserRouter([
 	{
 		path: "/",
 		Component: Home,
 		loader: homeLoader,
+		action: homeAction,
 	},
 	{
 		path: "/login",
@@ -29,9 +29,5 @@ export const { router, href } = typesafeBrowserRouter([
 	{
 		path: "/terms",
 		Component: Terms,
-	},
-	{
-		path: "/feed",
-		Component: Feed,
 	},
 ]);
--- web/src/routes/Home.data.tsx
+++ web/src/routes/Home.data.tsx
@@ -1,5 +1,25 @@
 import { makeLoader } from "react-router-typesafe";
 
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const homeAction = async ({ request }: { request: Request }) => {
+	console.log("homeAction");
+	const formData = await request.formData();
+	const response = await fetch(`${BACKEND_URL}/user/search`, {
+		method: "POST",
+		mode: "no-cors",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: formData,
+	}).catch((err) => {
+		return {
+			error: "An error occurred",
+		} as const;
+	});
+	return response;
+};
+
 export const homeLoader = makeLoader(async ({ request }) => {
 	console.log("homeLoader");
 	return null;
