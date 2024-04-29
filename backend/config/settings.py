0d80a2aa1f4488eebedf16fcbfb48b3ff6081d52
Ozan Karakaya <ozank018@gmail.com>
2024-04-29 17:44:24 +0300

    fix(devops): add cors origins whitelist

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'user',
+    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'user.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +137,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:5175",
+    "https://localhost:5173",
+    "https://localhost:5175",
+]
