3601d860cd18a6f0c8651caa945cc29f4c8bc025
Ozan Karakaya <ozank018@gmail.com>
2024-05-12 16:10:34 +0300

    feat(backend): added test_token api under users in order to check the functionality of manual token validation inside view methods

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('signup', views.register, name="signup"),
     path('login', views.login, name="login"),
+    path('test_token/', views.test_token, name="test_token"),
 ]
