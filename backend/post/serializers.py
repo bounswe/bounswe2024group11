ef071ef98df8f084e6507de562101b5f667dde21
Memin Seeker <72305188+Meminseeker@users.noreply.github.com>
2024-05-12 15:40:19 +0300

    feat: add serializers for Post, Like, and Bookmark models

---
+++ backend/post/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Post, Like, Bookmark
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class LikeSerializer(serializers.ModelSerializer):
+    # author = serializers.ReadOnlyField(source='user.username')
+    # post = serializers.ReadOnlyField(source='post.title')
+    class Meta:
+        model = Like
+        fields = '__all__'
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    # author = serializers.ReadOnlyField(source='user.username')
+    # post = serializers.ReadOnlyField(source='post.title')
+    class Meta:
+        model = Bookmark
+        fields = '__all__'
