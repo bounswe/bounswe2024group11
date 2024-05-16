24b90cc184868c6e17157fc5cfb8c95897c313c1
Memin Seeker <72305188+Meminseeker@users.noreply.github.com>
2024-05-16 16:33:24 +0300

    chore(backend): remove old backend

--- backend/user/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta(object):
-        model = User
-        fields = '__all__'
