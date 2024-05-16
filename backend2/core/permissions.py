a5bbd9f0d6c6846f3c4abc4984e601b96a7ba866
Yunus Kağan Aydın <133023181+yunuskaydin@users.noreply.github.com>
2024-05-16 11:47:11 +0300

    fix(backend): add authorization rules and unique constraints for like, bookmark and follow endpoints (#387)
    Co-authored-by: Memin Seeker <72305188+Meminseeker@users.noreply.github.com>
    Co-authored-by: MucahitErdoganUnlu <mucahit.719@hotmail.com>
    Co-authored-by: tenacke <emrekilic1010@gmail.com>

--- backend2/core/permissions.py
+++ backend2/core/permissions.py
@@ -1,9 +1,23 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsAuthorOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
         return obj.author == request.user
+    
+class IsUserOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user == request.user
+    
+class IsFollowerOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.follower == request.user
