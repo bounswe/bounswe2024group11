7a57f9b471172bc1d9d268667068f6b2cfd330bf
MucahitErdoganUnlu <mucahit.719@hotmail.com>
2024-05-16 18:15:57 +0300

    feat(backend): add swagger ui documentation for wiki-info

--- backend/swagger_docs/swagger.py
+++ backend/swagger_docs/swagger.py
@@ -133,3 +133,70 @@ login_swagger = {
         401: "Invalid credentials.",
     },
 }
+
+wiki_info_swagger = {
+        "manual_parameters":[
+            openapi.Parameter(
+                "qid",
+                openapi.IN_QUERY,
+                description="The QID (Query ID) of the Wikidata entity. "
+                            "QID is a unique identifier assigned to each item on Wikidata.",
+                type=openapi.TYPE_STRING,
+                required=True,
+            ),
+        ],
+        "responses":{
+            200: openapi.Response(
+                description="Successful retrieval of Wikidata information.",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        "keyword": openapi.Schema(
+                            type=openapi.TYPE_STRING,
+                            description="The QID used for the query.",
+                        ),
+                        "results": openapi.Schema(
+                            type=openapi.TYPE_ARRAY,
+                            description="Array of Wikidata entity information.",
+                            items=openapi.Schema(
+                                type=openapi.TYPE_OBJECT,
+                                properties={
+                                    "Inception": openapi.Schema(
+                                        type=openapi.TYPE_STRING,
+                                        description="The inception date of the Wikidata entity.",
+                                    ),
+                                    "Gender": openapi.Schema(
+                                        type=openapi.TYPE_STRING,
+                                        description="The gender of the Wikidata entity.",
+                                    ),
+                                    "Birth Name": openapi.Schema(
+                                        type=openapi.TYPE_STRING,
+                                        description="The birth name of the Wikidata entity.",
+                                    ),
+                                    "Place of Birth": openapi.Schema(
+                                        type=openapi.TYPE_STRING,
+                                        description="The place of birth of the Wikidata entity.",
+                                    ),
+                                    "Image": openapi.Schema(
+                                        type=openapi.TYPE_STRING,
+                                        description="The URL of the image associated with the Wikidata entity.",
+                                    ),
+                                    "Description": openapi.Schema(
+                                        type=openapi.TYPE_STRING,
+                                        description="A brief description of the Wikidata entity.",
+                                    ),
+                                    "Label": openapi.Schema(
+                                        type=openapi.TYPE_STRING,
+                                        description="The label or name of the Wikidata entity.",
+                                    ),
+                                }
+                            )
+                        )
+                    }
+                )
+            ),
+            400: "Bad Request: Missing or invalid QID parameter.",
+            404: "Not Found: The specified Wikidata entity does not exist.",
+            500: "Internal Server Error: An unexpected error occurred."
+        },   
+} 
