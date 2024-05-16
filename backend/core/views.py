341eaf2e84eef4c623372167b99c22e7cf5e8eb9
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-05-16 18:07:20 +0300

    feat(backend): implement wiki info view

--- backend/core/views.py
+++ backend/core/views.py
@@ -223,3 +223,24 @@ class ProfileViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+class WikiInfoView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    # @swagger_auto_schema(**wiki_info_swagger)
+    def get(self, request):
+        qid = request.query_params.get("qid")
+        if not qid:
+            return Response(
+                {"res": 'Parameter "qid" is required.'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            result_data = wikidata_helpers.wiki_info_helper(qid)
+            return Response(result_data.data)
+        except Exception as e:
+            return Response(
+                {"res": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
