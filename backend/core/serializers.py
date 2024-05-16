d17e46434a769977c31468d3518073cbbd08dbe2
Memin Seeker <72305188+Meminseeker@users.noreply.github.com>
2024-05-17 02:18:00 +0300

    feat(backend): update serializers to include user and profile fields

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -3,7 +3,82 @@ from django.contrib.auth.models import User
 from .models import Post, Like, Bookmark, Follow, Profile
 
 
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        read_only_fields = ['user']
+        fields = '__all__'
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookmark
+        read_only_fields = ['user']
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follow
+        read_only_fields = ['follower']
+        fields = '__all__'
+        read_only_fields = ['follower']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    fullname = serializers.SerializerMethodField()
+
+    def get_fullname(self, obj):
+        return obj.first_name
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'fullname']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    fullname = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', "fullname"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['fullname'],
+        )
+        Profile.objects.create(owner=user)
+        return user
+
+class ProfileSerializer(serializers.ModelSerializer):
+    is_following = serializers.SerializerMethodField()
+    def get_is_following(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return obj.owner.followers.filter(follower=request.user).exists()
+        return False
+    
+    class Meta:
+        model = Profile
+        read_only_fields = ["owner"]
+        fields = '__all__'
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    picture = serializers.URLField(source="profile.picture", allow_null=True)
+    biography = serializers.CharField(source="profile.biography", allow_blank=True)
+    fullname = serializers.CharField(source="first_name", allow_blank=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'fullname', 'picture', 'biography']
+
+
 class CreatePostSerializer(serializers.ModelSerializer):
+    author_profile = UserProfileSerializer(source='author', read_only=True)
     like_count = serializers.SerializerMethodField()
     bookmark_count = serializers.SerializerMethodField()
     liked_by = serializers.SerializerMethodField()
@@ -89,78 +164,3 @@ class SearchPostSerializer(serializers.ModelSerializer):
         model = Post
         exclude = ['author']
         read_only_fields = ["username", "title", "content", "image_src", "qid", "qtitle", "created_at", "updated_at", "like_count", "bookmark_count", "liked_by", "is_liked", "is_bookmarked", "user_id", "is_following"]
-
-
-class LikeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Like
-        read_only_fields = ['user']
-        fields = '__all__'
-
-
-class BookmarkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Bookmark
-        read_only_fields = ['user']
-        fields = '__all__'
-
-
-class FollowSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Follow
-        read_only_fields = ['follower']
-        fields = '__all__'
-        read_only_fields = ['follower']
-
-
-class UserSerializer(serializers.ModelSerializer):
-    fullname = serializers.SerializerMethodField()
-
-    def get_fullname(self, obj):
-        return obj.first_name
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'fullname']
-
-
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    fullname = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password', "fullname"]
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            first_name=validated_data['fullname'],
-        )
-        Profile.objects.create(owner=user)
-        return user
-
-class ProfileSerializer(serializers.ModelSerializer):
-    is_following = serializers.SerializerMethodField()
-    def get_is_following(self, obj):
-        request = self.context.get('request')
-        if request and request.user.is_authenticated:
-            return obj.owner.followers.filter(follower=request.user).exists()
-        return False
-    
-    class Meta:
-        model = Profile
-        read_only_fields = ["owner"]
-        fields = '__all__'
-
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    picture = serializers.URLField(source="profile.picture", allow_null=True)
-    biography = serializers.CharField(source="profile.biography", allow_blank=True)
-    fullname = serializers.CharField(source="first_name", allow_blank=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'fullname', 'picture', 'biography']
-    
