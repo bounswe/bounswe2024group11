0d25f0f675bd7a65a53ff1ea14112b2726b18daf
MucahitErdoganUnlu <mucahit.719@hotmail.com>
2024-10-12 01:42:06 +0300

    refactor(be): relocate views to views folder

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path
 
-from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
-from .views import RegisterView
+from .views import views
+from .views.jwt_views import DecoratedTokenObtainPairView, DecoratedTokenRefreshView, DecoratedTokenVerifyView, RegisterView
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/', DecoratedTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', DecoratedTokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', DecoratedTokenVerifyView.as_view(), name='token_verify'),
     path('register/', RegisterView.as_view(), name='register'),
 
 ]
