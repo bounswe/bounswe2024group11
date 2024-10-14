7a7a96dd37613d8bc732fdfd9d8cea34c305e33d
MucahitErdoganUnlu <mucahit.719@hotmail.com>
2024-10-14 20:40:29 +0300

    docs(be): add /swagger.yaml/ path to generate yaml file for swagger documentation

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.urls import re_path
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -36,5 +37,7 @@ urlpatterns = [
     path("api/v1/", include("core.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+
 
 ]
