1587d76f19cc291c2a6b9309d87e7b644b22b048
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-10-17 20:22:26 +0300

    feat(mobile): add Forum screen navigation
    Co-authored-by: Memin Seeker <muhammed.arayici@boun.edu.tr>

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { AuthProvider, useAuth } from './app/context/AuthContext';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './app/screens/Home';
-import Login from './app/screens/Login';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { AuthProvider, useAuth } from "./app/context/AuthContext";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./app/screens/Home";
+import Login from "./app/screens/Login";
+import { Forum } from "./app/screens/Forum";
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +23,7 @@ export const Layout = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        {/*
         {authState?.authenticated
           ? <Stack.Screen
             name='Home'
@@ -30,7 +32,9 @@ export const Layout = () => {
               headerRight: () => <Button onPress={onLogout} title="Sign Out" />
             }}></Stack.Screen>
           : <Stack.Screen name='Login' component={Login}></Stack.Screen>}
+          */}
+        <Stack.Screen name="Forum" component={Forum} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
