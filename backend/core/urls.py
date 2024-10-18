827f65b3aaf22cf1cc16e9f03223537521632a59
MucahitErdoganUnlu <mucahit.719@hotmail.com>
2024-10-18 16:02:52 +0300

    fix(be): be consistent with the urls in development branch

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,21 +1,19 @@
 from django.urls import path
-
-from .views import views
-from .views.jwt_views import DecoratedTokenObtainPairView, DecoratedTokenRefreshView, DecoratedTokenVerifyView, RegisterView
 from .views.forum_views import ForumQuestionViewSet
 from rest_framework.routers import DefaultRouter
+from .views import views
+from .views.jwt_views import DecoratedTokenObtainPairView, DecoratedTokenRefreshView, DecoratedTokenVerifyView, RegisterView
 
 router = DefaultRouter()
 router.register(r'forum-questions', ForumQuestionViewSet, basename='forum-question')
 
-
 urlpatterns = [
     path("", views.index, name="index"),
-    path('token/', DecoratedTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/login/', DecoratedTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', DecoratedTokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', DecoratedTokenVerifyView.as_view(), name='token_verify'),
-    path('register/', RegisterView.as_view(), name='register'),
-    
+    path('auth/register/', RegisterView.as_view(), name='register'),
+
 ]
 
 urlpatterns += router.urls
