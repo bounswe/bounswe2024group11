2fad5f2a911715e0e5ba830dc1688529bb80e211
Memin Seeker <muhammed.arayici@boun.edu.tr>
2024-10-19 02:23:18 +0300

    refactor(mobile): refactor ForumAnswerCard component to use StyleSheet

--- mobile/app/components/ForumAnswerCard.tsx
+++ mobile/app/components/ForumAnswerCard.tsx
@@ -1,10 +1,11 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { Card, Button } from 'react-native-paper'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { Answer } from '../types/forum'
 import VoteButtonsView from './VoteButtonsView'
 import AuthorView from './AuthorView'
+import { getRelativeTime } from '../utils'
 
 interface ForumAnswerCardProps {
     item: Answer;
@@ -14,8 +15,10 @@ const ForumAnswerCard: React.FC<ForumAnswerCardProps> = ({ item }) => {
     return (
         <View>
             <Card style={{ borderRadius: 10, marginBottom: 10, padding: 10 }}>
-                <View>
+                <View style={styles.container}>
                     <AuthorView author={item.author} />
+                    {/* <Text style={styles.posted_time}>Posted {getRelativeTime(new Date(item.created_at))}</Text> */}
+                    <Text style={styles.posted_time}>Posted</Text>
                 </View>
                 <Text>{item.body}</Text>
                 <VoteButtonsView is_upvoted={item.is_upvoted} upvotes_count={item.upvotes_count} is_downvoted={item.is_downvoted} downvotes_count={item.downvotes_count} />
@@ -23,5 +26,19 @@ const ForumAnswerCard: React.FC<ForumAnswerCardProps> = ({ item }) => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        // alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+    posted_time: {
+        color: 'grey',
+        marginLeft: 10,
+        // justifyContent: 'flex-end',
+    }
+})
 
 export default ForumAnswerCard
