a6bbb37d93e77859d74163bd254370df8c9647de
Memin Seeker <muhammed.arayici@boun.edu.tr>
2024-10-18 23:18:10 +0300

    feat(mobile): add initial ForumQuestionDetail component and add route prop

--- mobile/app/screens/ForumQuestionDetail.tsx
+++ mobile/app/screens/ForumQuestionDetail.tsx
@@ -1,7 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { RouteProp } from '@react-navigation/native'
+import { RootStackParamList } from '../../App'
 
-const ForumQuestionDetail = () => {
+type ForumQuestionDetailScreenRouteProp = RouteProp<RootStackParamList, 'ForumQuestionDetail'>;
+
+type Props = {
+  route: ForumQuestionDetailScreenRouteProp;
+};
+
+const ForumQuestionDetail: React.FC<Props> = ({ route }) => {
   return (
     <View>
       <Text>ForumQuestionDetail</Text>
