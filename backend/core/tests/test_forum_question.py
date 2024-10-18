49812f54e02a8a02ee7fadae3c5fe3633b3b64e2
MucahitErdoganUnlu <mucahit.719@hotmail.com>
2024-10-18 16:01:23 +0300

    test(be): update forum question tests to reflect the change in tag-question many-to-many relationship

--- backend/core/tests/test_forum_question.py
+++ backend/core/tests/test_forum_question.py
@@ -16,40 +16,66 @@ class ForumQuestionSetup(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username=Faker().user_name(), password=Faker().password(),
                                              email=Faker().email(), full_name=Faker().name())
-        self.forum_question = ForumQuestion.objects.create(title='Test Question', question='What is the meaning of life?',
-                                                              tag='philosophy', author=self.user)
         self.client = APIClient()
+        refresh = RefreshToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(refresh.access_token)}')
+
+        self.data = {
+            "title": "How to serialize ManyToMany relationships?",
+            "question": "I need help with ManyToMany relationships in Django Rest Framework.",
+            "tags": [
+                {"name": "Django", "linked_data_id": "123", "description": "A web framework."},
+                {"name": "DRF", "linked_data_id": "456", "description": "Django Rest Framework."}
+            ]
+        }
+
+        
+        self.client.post(reverse('forum-question-list'), self.data, format='json')
+        self.forum_question = ForumQuestion.objects.get(title='How to serialize ManyToMany relationships?')
+
 
 class ForumQuestionTestCase(ForumQuestionSetup):
     
     def test_forum_question_creation(self):
-        response = self.client.get(reverse('forum-question-list'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data[0]['title'], self.forum_question.title)
-        self.assertEqual(response.data[0]['question'], self.forum_question.question)
-        self.assertEqual(response.data[0]['tag'], self.forum_question.tag)
-        self.assertEqual(response.data[0]['author'], self.forum_question.author.id)
+        data = {
+            "title": "How to serialize ManyToMany relationships?",
+            "question": "I need help with ManyToMany relationships in Django Rest Framework.",
+            "tags": [
+                {"name": "Django", "linked_data_id": "123", "description": "A web framework."},
+                {"name": "DRF", "linked_data_id": "456", "description": "Django Rest Framework."}
+            ]
+        }
+
+        
+        response = self.client.post(reverse('forum-question-list'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ForumQuestion.objects.count(), 2)
+        self.assertEqual(ForumQuestion.objects.all()[1].title, data['title'])
+        self.assertEqual(ForumQuestion.objects.all()[1].question, data['question'])
+        self.assertEqual(ForumQuestion.objects.all()[1].tags.count(), 2)
+
 
     def test_forum_question_update(self):
-        refresh = RefreshToken.for_user(self.user)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(refresh.access_token)}')
+
+        data = {
+            "title": "New Title",
+            "question": "I need help with ManyToMany relationships in Django Rest Framework.",
+            "tags": [
+                {"name": "Django", "linked_data_id": "123", "description": "A web framework."},
+                {"name": "DRF", "linked_data_id": "456", "description": "Django Rest Framework."}
+            ]
+        }
 
         response = self.client.put(reverse('forum-question-detail', args=[self.forum_question.id]),
-                                   {'title': 'Updated Question', 'question': 'What is the meaning of life?', 'tag': 'philosophy'}
+                                   data, format='json'
                                    )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'Updated Question')
+        self.assertEqual(response.data['title'], data['title'])
 
     def test_forum_question_delete(self):
         refresh = RefreshToken.for_user(self.user)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(refresh.access_token)}')
         self.client.delete(reverse('forum-question-detail', args=[self.forum_question.id]))
 
-    def test_forum_question_list(self):
-        ForumQuestion.objects.create(title='Test Question 2', question='What is the meaning of life?', tag='philosophy', author=self.user)
-        ForumQuestion.objects.create(title='Test Question 3', question='What is the meaning of life?', tag='philosophy', author=self.user)
-        response = self.client.get(reverse('forum-question-list'))
-        self.assertEqual(len(response.data), 3)
 
     def test_user_cannot_modify_other_users_questions(self):
         user2 = User.objects.create_user(username=Faker().user_name(), password=Faker().password(),
@@ -65,6 +91,7 @@ class ForumQuestionTestCase(ForumQuestionSetup):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_unlogged_user_cannot_create_question(self):
-        response = self.client.post(reverse('forum-question-list'), {'title': 'Test Question 2', 'question': 'What is the meaning of life?', 'tag': 'philosophy'})
+        self.client.credentials()
+        response = self.client.post(reverse('forum-question-list'), self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
