dc5cfd62b1b839485b9f66bbf6215c9bb1a84b53
Mücahit Erdoğan Ünlü <94074126+MucahitErdoganUnlu@users.noreply.github.com>
2024-10-19 23:53:58 +0300

    feat(be): implement forum-question endpoint and tagging endpoint (#546)
    Co-authored-by: meminciftci <meminciftci41@gmail.com>
    Co-authored-by: Ceydanur Şen <ceydasen40@gmail.com>

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,6 +137,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 AUTH_USER_MODEL = 'core.CustomUser'
