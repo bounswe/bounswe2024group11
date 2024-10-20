0c29704a1e18cce6f40c03f7d1ef750bbdc804e7
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-10-20 18:47:25 +0300

    refactor(mobile): update navigation and add Register screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
 import { AuthProvider, useAuth } from './app/context/AuthContext';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './app/screens/Home';
 import Login from './app/screens/Login';
+import Register from './app/screens/Register';  // import the new Register screen
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <AuthProvider>
-      <Layout></Layout>
+      <Layout />
     </AuthProvider>
   );
 }
@@ -22,15 +23,22 @@ export const Layout = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {authState?.authenticated
-          ? <Stack.Screen
-            name='Home'
+        {authState?.authenticated ? (
+          <Stack.Screen
+            name="Home"
             component={Home}
             options={{
-              headerRight: () => <Button onPress={onLogout} title="Sign Out" />
-            }}></Stack.Screen>
-          : <Stack.Screen name='Login' component={Login}></Stack.Screen>}
+              headerRight: () => <Button onPress={onLogout} title="Sign Out" />,
+            }}
+          />
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
+
