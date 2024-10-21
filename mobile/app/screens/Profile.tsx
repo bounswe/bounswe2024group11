5c5c1db89b004341bc0646060706a176efb7f5f2
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-10-21 21:47:02 +0300

    feat(mobile): add Profile screen

---
+++ mobile/app/screens/Profile.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Profile() {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
