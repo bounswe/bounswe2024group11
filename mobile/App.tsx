fe39cb3ab8ff1f3859953d5477acc3214401933e
Yunus Kağan Aydın <133023181+yunuskaydin@users.noreply.github.com>
2024-10-22 01:00:57 +0300

    feat(mobile): add bottom Tab (#584)
    * feat(mobile): add @react-navigation/bottom-tabs dependency
    
    * feat(mobile): add @react-navigation/bottom-tabs dependency
    
    * feat(mobile): add bottom tab navigation
    
    * feat(mobile): add Leaderboard screen
    
    * feat(mobile): add Profile screen
    
    * feat(mobile): add Quiz screen
    
    * feat(mobile): add profile icon image
    
    * chore(mobile): add styles to placeholder Leaderboard, Profile, and Quiz screens
    
    ---------
    
    Co-authored-by: Memin Seeker <muhammed.arayici@boun.edu.tr>

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,23 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { Button } from "react-native";
+import { Button, Image, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { AuthProvider, useAuth } from "./app/context/AuthContext";
 import CreateQuestion from "./app/screens/CreateQuestion";
 import Forum from "./app/screens/Forum";
 import ForumQuestionDetail from "./app/screens/ForumQuestionDetail";
-import Home from "./app/screens/Home";
+import Leaderboard from "./app/screens/Leaderboard";
 import Login from "./app/screens/Login";
+import Profile from "./app/screens/Profile";
+import Quiz from "./app/screens/Quiz";
 import Register from "./app/screens/Register"; // import the new Register screen
 import { Question } from "./app/types/forum";
 
 export type RootStackParamList = {
-  Home: undefined;
   Login: undefined;
+  MainTabs: undefined; // Use this for the bottom tabs
   Register: undefined;
   Forum: undefined;
   ForumQuestionDetail: { question: Question };
@@ -22,48 +25,113 @@ export type RootStackParamList = {
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <AuthProvider>
-        <Layout></Layout>
+        <Layout />
       </AuthProvider>
     </GestureHandlerRootView>
   );
 };
 
+const BottomTabNavigator = () => {
+  const { onLogout } = useAuth();
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Forum"
+        component={Forum}
+        options={{
+          headerRight: () => (
+            <View style={{ marginRight: 10 }}>
+              <Button onPress={onLogout} title="Sign Out" />
+            </View>
+          ),
+          tabBarIcon: () => (
+            <Image
+              source={require("./assets/forum_icon.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Quiz"
+        component={Quiz}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("./assets/quiz_icon.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Leaderboard"
+        component={Leaderboard}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("./assets/leaderboard_icon.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("./assets/profile_icon.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
 export const Layout = () => {
-  const { authState, onLogout } = useAuth();
+  const { authState } = useAuth();
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/*
-        {authState?.authenticated
-          ? <Stack.Screen
-            name='Home'
-            component={Home}
-            options={{
-              headerRight: () => <Button onPress={onLogout} title="Sign Out" />
-            }}></Stack.Screen>
-          : <Stack.Screen name='Login' component={Login}></Stack.Screen>}
-          */}
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerRight: () => <Button onPress={onLogout} title="Sign Out" />,
-          }}
-        />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Forum" component={Forum} />
-        <Stack.Screen name="CreateQuestion" component={CreateQuestion} />
-        <Stack.Screen
-          name="ForumQuestionDetail"
-          component={ForumQuestionDetail}
-        />
+        {authState?.authenticated ? (
+          <>
+            <Stack.Screen
+              name="MainTabs"
+              component={BottomTabNavigator}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Forum" component={Forum} />
+            <Stack.Screen name="CreateQuestion" component={CreateQuestion} />
+            <Stack.Screen
+              name="ForumQuestionDetail"
+              component={ForumQuestionDetail}
+            />
+            <Stack.Screen name="Register" component={Register} />
+            {/*
+            {authState?.authenticated
+              ? <Stack.Screen
+                name='Home'
+                component={Home}
+                options={{
+                  headerRight: () => <Button onPress={onLogout} title="Sign Out" />
+                }}></Stack.Screen>
+              : <Stack.Screen name='Login' component={Login}></Stack.Screen>}
+              */}
+          </>
+        ) : (
+          <Stack.Screen name="Login" component={Login} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
