c0096c7ac584636c7b97910e6a4c78b92d9ee28c
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-10-21 21:47:10 +0300

    feat(mobile): add Quiz screen

---
+++ mobile/app/screens/Quiz.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Quiz() {
+  return (
+    <View>
+      <Text>Quiz</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
