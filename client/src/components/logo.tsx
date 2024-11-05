fb965d0cc2b4908e3708e520faf1eca87722d003
Hasan Kerem Şeker <95296467+hks1444@users.noreply.github.com>
2024-11-05 22:35:50 +0300

    w3c(client): apply standards initiatives (#595)
    * feature(client): remove onClick and div to Link in Forumcard
    
    * feature(client): modified aria label for forum-card component and added aria label for upvote and down-vote buttons
    
    * coauthored
    
    Co-authored-by: Ümit Can Evleksiz <evleksizumitcan@gmail.com>
    
    * add standard initiatives
    
    * refine the wording
    
    Co-authored-by: meminciftci <meminciftci41@gmail.com>
    Co-authored-by: ArdaVural <ardavural09@gmail.com>
    
    * fix logo svg a11y
    
    Co-authored-by: meminciftci <meminciftci41@gmail.com>
    
    ---------
    
    Co-authored-by: Ümit Can Evleksiz <evleksizumitcan@gmail.com>
    Co-authored-by: meminciftci <meminciftci41@gmail.com>
    Co-authored-by: ArdaVural <ardavural09@gmail.com>

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -10,7 +10,9 @@ export const Logo = ({ size = 40 }: LogoProps) => {
             viewBox="0 0 40 40"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            aria-labelledby="logo-title"
         >
+            <title id="logo-title">Turquiz</title>
             <use href="#logo-shape" />
         </svg>
     );
