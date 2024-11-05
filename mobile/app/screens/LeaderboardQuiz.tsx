156675e8a2ea59c57bbae79886df8f27bdc7f86f
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-11-05 15:49:46 +0300

    feat(mobile): initialize LeaderboardQuiz screen component

---
+++ mobile/app/screens/LeaderboardQuiz.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function LeaderboardQuiz() {
+  return (
+    <View>
+      <Text>LeaderboardQuiz</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
