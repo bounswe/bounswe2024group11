abc0e3f140b9ca3f48fbe4e0e4e3ae516f178ba9
Memin Seeker <muhammed.arayici@boun.edu.tr>
2024-11-05 23:15:13 +0300

    feat(mobile): add QuizCardHeader component for displaying quiz difficulty and rating score

---
+++ mobile/app/components/QuizCardHeader.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { QuizCardHeaderType } from "../types/quiz";
+
+interface QuizCardHeaderProps {
+  item: QuizCardHeaderType;
+}
+
+const QuizCardHeader: React.FC<QuizCardHeaderProps> = ({ item }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.difficulty_card}>
+        <Text style={styles.difficulty_text}>{item.difficulty}</Text>
+      </View>
+      <View style={styles.rating_score_card}>
+        <Text style={styles.rating_score_text}>{item.rating_score}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 12,
+  },
+  difficulty_card: {
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    margin: 4,
+    backgroundColor: "#5DA3FA",
+    elevation: 4,
+  },
+  difficulty_text: {
+    fontSize: 16,
+  },
+  rating_score_card: {
+    borderRadius: 4,
+    padding: 4,
+    backgroundColor: "#50DBB4",
+    elevation: 4,
+  },
+  rating_score_text: {
+    fontSize: 20,
+  },
+});
+
+export default QuizCardHeader;
