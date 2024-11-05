3ba980cc5f8b98bb194f2eadfd22fd67b10c97ef
Memin Seeker <muhammed.arayici@boun.edu.tr>
2024-11-05 23:17:16 +0300

    feat(mobile): implement Quiz screen with quiz fetching and display using QuizCard component

--- mobile/app/screens/Quiz.tsx
+++ mobile/app/screens/Quiz.tsx
@@ -1,22 +1,78 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useIsFocused, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { RootStackParamList } from "../../App";
+import QuizCard from "../components/QuizCard";
+import { QuizOverview } from "../types/quiz";
 
-export default function Quiz() {
+const API_URL = "http://10.0.2.2:3000/quiz-feed";
+
+type ForumScreenNavigationProp = NativeStackNavigationProp<RootStackParamList>;
+
+const Quiz: React.FC = () => {
+  const [quizzes, setQuizzes] = useState<QuizOverview[]>([]);
+
+  const navigation = useNavigation<ForumScreenNavigationProp>();
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const result = await axios.get(`${API_URL}`);
+        setQuizzes(result.data);
+      } catch (error) {
+        console.error("Error fetching questions", error);
+      }
+    };
+
+    if (isFocused) {
+      fetchQuestions();
+    }
+  }, [isFocused]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Quizzes</Text>
+      <Text style={styles.title}>Quizzes</Text>
+      <Text style={styles.titleText}>
+        Test your knowledge of various topics.
+      </Text>
+      <FlatList
+        data={quizzes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <QuizCard item={item} />
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    // justifyContent: "center",
+    // alignItems: "center",
+    padding: 24,
   },
-  text: {
-    fontSize: 24,
+  title: {
+    fontSize: 36,
     fontWeight: "bold",
   },
+  titleText: {
+    fontSize: 16,
+    fontWeight: "normal",
+    marginBottom: 24,
+  },
 });
+
+export default Quiz;
