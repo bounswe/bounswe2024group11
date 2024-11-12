d30d312500eef1b9d7ee8a59a041ef03ce954172
Memin Seeker <muhammed.arayici@boun.edu.tr>
2024-11-12 15:57:22 +0300

    feat(mobile): add initial QuizDetail screen for displaying detailed quiz information

---
+++ mobile/app/screens/QuizDetail.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function QuizDetail() {
+  return (
+    <View>
+      <Text>QuizDetail</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
