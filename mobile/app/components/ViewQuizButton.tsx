a7249da06f6a06c85374d9a1d163b538db3c8768
Memin Seeker <muhammed.arayici@boun.edu.tr>
2024-11-18 23:54:02 +0300

    feat(mobile): enhance ViewQuizButton to navigate to ViewQuiz with quiz details and improve styling

--- mobile/app/components/ViewQuizButton.tsx
+++ mobile/app/components/ViewQuizButton.tsx
@@ -1,20 +1,43 @@
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { RootStackParamList } from "../../App";
 
 interface ViewQuizButtonProps {
   item: {
     id: string;
+    type: number;
+    title: string;
+    description: string;
     is_taken: boolean;
   };
 }
 
+type TakeQuizNavigationProp = NativeStackNavigationProp<RootStackParamList>;
+
 const ViewQuizButton: React.FC<ViewQuizButtonProps> = ({ item }) => {
+  const navigation = useNavigation<TakeQuizNavigationProp>();
+
   return (
     <View style={styles.container}>
       <Text style={styles.button}>
         {item.is_taken ? "Review Quiz" : "Take Quiz"}
       </Text>
-      {item.is_taken && <Text style={styles.secondButton}>Retake Quiz</Text>}
+      {item.is_taken && (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("ViewQuiz", {
+              id: item.id,
+              type: item.type,
+              title: item.title,
+              description: item.description,
+            })
+          }
+        >
+          <Text style={styles.secondButton}>Retake Quiz</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
