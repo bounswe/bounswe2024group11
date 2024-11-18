6a8e7adc6bdebf3e353f32e214b4730e34c50e4d
Memin Seeker <muhammed.arayici@boun.edu.tr>
2024-11-18 16:31:31 +0300

    feat: add styling to the take quiz button in quiz detail screen

--- mobile/app/components/ViewQuizButton.tsx
+++ mobile/app/components/ViewQuizButton.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Button } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
 
 interface ViewQuizButtonProps {
   item: {
@@ -11,12 +10,41 @@ interface ViewQuizButtonProps {
 
 const ViewQuizButton: React.FC<ViewQuizButtonProps> = ({ item }) => {
   return (
-    <View>
-      <Button>{item.is_taken ? "Review Quiz" : "Take Quiz"}</Button>
+    <View style={styles.container}>
+      <Text style={styles.button}>
+        {item.is_taken ? "Review Quiz" : "Take Quiz"}
+      </Text>
+      {item.is_taken && <Text style={styles.secondButton}>Retake Quiz</Text>}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+  button: {
+    backgroundColor: "#22D3EE",
+    color: "#ffffff",
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 12,
+    borderRadius: 8,
+    marginVertical: 12,
+    elevation: 4,
+  },
+  secondButton: {
+    backgroundColor: "#000000",
+    color: "#ffffff",
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 12,
+    borderRadius: 8,
+    marginVertical: 12,
+    elevation: 4,
+  },
+});
 
 export default ViewQuizButton;
