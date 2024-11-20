470827bcdcb34038bbc6fdfd5d6565fa77ce52fe
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-11-20 19:01:10 +0300

    feat(mobile): implement initial voting functionality in VoteButtonsView component

--- mobile/app/components/VoteButtonsView.tsx
+++ mobile/app/components/VoteButtonsView.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import axios from "axios";
 
 interface VoteButtonViewsProps {
   is_upvoted: boolean;
   upvotes_count: number;
   is_downvoted: boolean;
   downvotes_count: number;
+  questionId: number;
+  onVoteChange: (is_upvoted: boolean, is_downvoted: boolean) => void;
 }
 
 const VoteButtonsView: React.FC<VoteButtonViewsProps> = ({
@@ -14,19 +17,63 @@ const VoteButtonsView: React.FC<VoteButtonViewsProps> = ({
   upvotes_count,
   is_downvoted,
   downvotes_count,
+  questionId,
+  onVoteChange,
 }) => {
+  const handleUpvote = async () => {
+    try {
+      if (is_upvoted) {
+        await axios.delete(
+          `http://54.247.125.93/api/v1/forum-upvote/${questionId}/`
+        );
+        onVoteChange(false, is_downvoted);
+      } else {
+        await axios.post("http://54.247.125.93/api/v1/forum-upvote/", {
+          forum_question: questionId,
+        });
+        onVoteChange(true, false);
+      }
+    } catch (error) {
+      console.error("Error upvoting:", error);
+    }
+  };
+
+  const handleDownvote = async () => {
+    try {
+      if (is_downvoted) {
+        await axios.delete(
+          `http://54.247.125.93/api/v1/forum-upvote/${questionId}/`
+        );
+        onVoteChange(is_upvoted, false);
+      } else {
+        await axios.post("http://54.247.125.93/api/v1/forum-upvote/", {
+          forum_question: questionId,
+        });
+        onVoteChange(false, true);
+      }
+    } catch (error) {
+      console.error("Error downvoting:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TouchableOpacity
         style={[styles.iconButton, is_upvoted && styles.upvotedButton]}
+        onPress={handleUpvote}
       >
-        <Icon name="arrow-up" size={24} color="#000" />
+        <Icon name="arrow-up" size={24} color={is_upvoted ? "#fff" : "#000"} />
       </TouchableOpacity>
       <Text style={styles.votesCount}>{upvotes_count - downvotes_count}</Text>
       <TouchableOpacity
         style={[styles.iconButton, is_downvoted && styles.downvotedButton]}
+        onPress={handleDownvote}
       >
-        <Icon name="arrow-down" size={24} color="#000" />
+        <Icon
+          name="arrow-down"
+          size={24}
+          color={is_downvoted ? "#fff" : "#000"}
+        />
       </TouchableOpacity>
     </View>
   );
