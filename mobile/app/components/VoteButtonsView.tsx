972c0e611cf93ed1677d35f60a51ea9e3b52403d
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-11-21 13:19:49 +0300

    fix(mobile): enhance vote handling to toggle between upvote and downvote states

--- mobile/app/components/VoteButtonsView.tsx
+++ mobile/app/components/VoteButtonsView.tsx
@@ -31,6 +31,18 @@ const VoteButtonsView: React.FC<VoteButtonViewsProps> = ({
         );
         console.log("Upvote deleted:", response.data);
         onVoteChange(null, null); // Clear upvote state
+      } else if (is_downvoted) {
+        const response = await axios.delete(
+          `http://54.247.125.93/api/v1/forum-downvote/${is_downvoted}/`
+        );
+        console.log("Downvote deleted:", response.data);
+        onVoteChange(null, null); // Clear downvote state
+        const response2 = await axios.post(
+          `http://54.247.125.93/api/v1/forum-upvote/`,
+          { forum_question: questionId }
+        );
+        console.log("Upvote added:", response2.data);
+        onVoteChange(response2.data.id, null); // Update upvote state
       } else {
         const response = await axios.post(
           `http://54.247.125.93/api/v1/forum-upvote/`,
@@ -52,6 +64,18 @@ const VoteButtonsView: React.FC<VoteButtonViewsProps> = ({
         );
         console.log("Downvote deleted:", response.data);
         onVoteChange(null, null); // Clear downvote state
+      } else if (is_upvoted) {
+        const response = await axios.delete(
+          `http://54.247.125.93/api/v1/forum-upvote/${is_upvoted}/`
+        );
+        console.log("Upvote deleted:", response.data);
+        onVoteChange(null, null); // Clear upvote state
+        const response2 = await axios.post(
+          `http://54.247.125.93/api/v1/forum-downvote/`,
+          { forum_question: questionId }
+        );
+        console.log("Downvote added:", response2.data);
+        onVoteChange(null, response2.data.id); // Update downvote state
       } else {
         const response = await axios.post(
           `http://54.247.125.93/api/v1/forum-downvote/`,
