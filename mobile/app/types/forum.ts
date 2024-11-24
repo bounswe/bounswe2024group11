09efccb1d6af32e55d1777ebf950f9ae95c6c14e
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-11-24 16:50:52 +0300

    fix(mobile): change is_upvoted and is_downvoted types to number | null in Answer type

--- mobile/app/types/forum.ts
+++ mobile/app/types/forum.ts
@@ -33,9 +33,9 @@ export type Answer = {
   body: string;
   author: Author;
   created_at: string;
-  is_upvoted: boolean;
+  is_upvoted: number | null;
   upvotes_count: number;
-  is_downvoted: boolean;
+  is_downvoted: number | null;
   downvotes_count: number;
   answers?: Answer[];
 };
