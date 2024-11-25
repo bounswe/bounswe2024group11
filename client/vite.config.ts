77e953da327e156a03c82f1785007ea402125f46
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-11-25 15:07:55 +0300

    hotfix(client): vite config out directory

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: { outDir: "/dist" },
+    build: { outDir: "dist" },
     plugins: [react()],
 });
