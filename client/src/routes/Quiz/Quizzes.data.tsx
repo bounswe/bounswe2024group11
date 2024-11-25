77ca92ea8a8c0390978899b97f0795a00ad734e3
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-11-25 22:14:10 +0300

    hotfix(client): set default per page param to 10 in loader

--- client/src/routes/Quiz/Quizzes.data.tsx
+++ client/src/routes/Quiz/Quizzes.data.tsx
@@ -15,7 +15,7 @@ const quizzesResponseSchema = object({
 export const quizzesLoader = (async ({ request }) => {
     const url = new URL(request.url);
     const page = Number(url.searchParams.get("page")) || 1;
-    const per_page = Number(url.searchParams.get("per_page")) || 20;
+    const per_page = Number(url.searchParams.get("per_page")) || 10;
 
     try {
         const response = await apiClient.get("/quizzes/", {
