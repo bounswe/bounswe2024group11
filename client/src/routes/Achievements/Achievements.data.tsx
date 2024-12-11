db9f84bf1c8c42524c41212fc84cf0e058b24ae4
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-12-11 22:54:41 +0300

    add achievements route statically

---
+++ client/src/routes/Achievements/Achievements.data.tsx
@@ -0,0 +1,5 @@
+import { LoaderFunction } from "react-router";
+
+export const achievementsLoader = (async () => {
+    return [];
+}) satisfies LoaderFunction;
---
+++ client/src/routes/Achievements/Achievements.tsx
@@ -0,0 +1,3 @@
+export const Achievements = () => {
+    return <div>All Achievements</div>;
+};
