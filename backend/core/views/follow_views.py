9300e6457eef60f5cb8281b2c93077ef6d8f0b42
Ozan Karakaya <ozank018@gmail.com>
2024-12-13 18:36:26 +0300

    feat(backend): enhance FollowViewSet with Swagger documentation for create, delete, and list operations

--- backend/core/views/follow_views.py
+++ backend/core/views/follow_views.py
@@ -2,15 +2,50 @@ from rest_framework import viewsets, permissions
 from ..models import Follow
 from ..serializers.serializers import FollowSerializer
 from rest_framework.exceptions import PermissionDenied
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class FollowViewSet(viewsets.ModelViewSet):
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     permission_classes = [permissions.IsAuthenticated]
-
+    http_method_names = ['get', 'post', 'delete']
+    
+    @swagger_auto_schema(
+        operation_summary="Create Follow",
+        request_body=FollowSerializer,
+        manual_parameters=[
+            openapi.Parameter(
+                'following',
+                openapi.IN_QUERY,
+                description="ID of the user to follow",
+                type=openapi.TYPE_INTEGER,
+                required=True
+            )],
+        responses={
+            201: FollowSerializer,
+            400: "Bad Request"
+        }
+    )
     def perform_create(self, serializer):
         serializer.save(follower=self.request.user)
 
+    @swagger_auto_schema(
+        operation_summary="Delete Follow",
+        manual_parameters=[
+            openapi.Parameter(
+                'id',
+                openapi.IN_PATH,
+                description="ID of the follow to delete",
+                type=openapi.TYPE_INTEGER,
+                required=True
+            )
+        ],
+        responses={
+            204: "No Content",
+            403: "Forbidden - Can only delete own follow"
+        }
+    )
     def perform_destroy(self, instance):
         # check if the user is trying to delete their own follow
         if instance.follower != self.request.user:
@@ -18,7 +53,12 @@ class FollowViewSet(viewsets.ModelViewSet):
         else:
             instance.delete()
             
-
+    @swagger_auto_schema(
+        operation_summary="List Follows",
+        responses={
+            200: FollowSerializer(many=True)
+        }
+    )
     def get_queryset(self):
         # Allow users to see only their own follows
         return self.queryset.filter(follower=self.request.user)
