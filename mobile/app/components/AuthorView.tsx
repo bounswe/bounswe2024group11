d0fd28c803dfc075f7862eecfe28fa0f96b786e6
Yunus Kağan Aydın <133023181+yunuskaydin@users.noreply.github.com>
2024-12-14 00:21:27 +0300

    feat(mobile): add UserCard and TabView to profile screen (#746)
    * fix(mobile): uncomment avatar field to LoggedinUser type
    
    * feat(mobile): add UserCard component for displaying user information
    
    * feat(mobile): add UserView component for displaying user details
    
    * feat(mobile): enhance Profile screen to display user information with UserCard component
    
    * feat(mobile): implement fetching and displaying bookmarked and upvoted questions' id and bookmark, upvote creation time in Profile screen
    
    * feat(mobile): enhance AuthorView to handle undefined author and provide default values for avatar and name
    
    * feat(mobile): handle undefined tags in ForumQuestionCard to prevent errors
    
    * feat(mobile): refactor Profile screen to fetch and display bookmarked and upvoted questions with improved error handling
    
    * feat(mobile): add my questions in Profile screen
    
    * fix(backend): add avatar field to response in login
    
    * feat(mobile): implement tabbed view for My, Bookmarked, and Upvoted questions in Profile screen
    
    * feat(mobile): add is_my_quiz field to QuizOverview for profile tab functionality
    
    * feat(mobile): add My Quizzes tab to Profile screen
    
    * feat(mobile): add My Taken Quizzes tab to Profile screen
    
    * feat(mobile): enable scrolling for tab bar in Profile screen
    
    * chore(mobile): update UserCard padding and margin for improved layout
    
    ---------
    
    Co-authored-by: Ümit Can Evleksiz <68506701+umitcan07@users.noreply.github.com>

--- mobile/app/components/AuthorView.tsx
+++ mobile/app/components/AuthorView.tsx
@@ -3,16 +3,28 @@ import { Image, StyleSheet, Text, View } from "react-native";
 import { Author } from "../types/forum";
 
 interface AuthorViewProps {
-  author: Author;
+  author: Author | undefined; // Allowing author to be possibly undefined
 }
 
 const AuthorView: React.FC<AuthorViewProps> = ({ author }) => {
+  // Ensure 'author' is defined and has the expected properties
+  if (!author) {
+    return null; // You can render a placeholder or return nothing if 'author' is missing
+  }
+
   return (
     <View style={styles.container}>
-      <Image source={{ uri: author.avatar }} style={styles.image} />
+      <Image
+        source={{ uri: author.avatar || "default-avatar-url" }} // Provide default URL in case avatar is missing
+        style={styles.image}
+      />
       <View>
-        <Text style={styles.full_name}>{author.full_name}</Text>
-        <Text style={styles.username}>@{author.username}</Text>
+        <Text style={styles.full_name}>
+          {author.full_name || "Unknown Author"}
+        </Text>
+        <Text
+          style={styles.username}
+        >{`@${author.username || "Unknown"}`}</Text>
       </View>
     </View>
   );
