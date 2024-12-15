b790ff0d49a280899205efeab715797b4ae07622
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-12-15 04:21:46 +0300

    feat(mobile): add initial follow/unfollow display to UserCard component

--- mobile/app/components/UserCard.tsx
+++ mobile/app/components/UserCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Card } from "react-native-paper";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { LoggedinUser } from "../types/user";
@@ -10,10 +10,40 @@ interface UserCardProps {
 }
 
 const UserCard: React.FC<UserCardProps> = ({ user }) => {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const handleFollowPress = () => {
+    setIsFollowing(!isFollowing);
+    // API CALL TO FOLLOW/UNFOLLOW WILL COME HERE
+    console.log(isFollowing ? "Unfollowed" : "Followed");
+  };
   return (
     <Card style={styles.card}>
       <View style={styles.container}>
         <UserView user={user} />
+        <View style={styles.followButton}>
+          <TouchableOpacity
+            onPress={handleFollowPress}
+            style={[
+              styles.button,
+              { backgroundColor: isFollowing ? "#ffffff" : "#5BADCE" }, // Dynamic background color
+            ]}
+          >
+            <Text
+              style={[
+                styles.buttonText,
+                { color: isFollowing ? "#5BADCE" : "#ffffff" },
+              ]}
+            >
+              {isFollowing ? "Following" : "Follow"}
+            </Text>
+            <Icon
+              name={isFollowing ? "account-check" : "account-plus"}
+              size={20}
+              color={isFollowing ? "#5BADCE" : "#ffffff"}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     </Card>
   );
@@ -27,10 +57,26 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "flex-start",
+    justifyContent: "space-between",
   },
   followButton: {
-    flex: 1,
-    alignItems: "flex-end",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#5BADCE",
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    borderRadius: 20,
+    borderWidth: 0.3,
+  },
+  buttonText: {
+    color: "#ffffff",
+    marginRight: 8,
+    fontWeight: "bold",
   },
 });
 
