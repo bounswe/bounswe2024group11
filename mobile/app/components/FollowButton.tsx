991d3a9af11ed104272efa27abc93d11e848868e
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-12-15 16:37:15 +0300

    feat(mobile): enhance FollowButton with icon and dynamic styling

--- mobile/app/components/FollowButton.tsx
+++ mobile/app/components/FollowButton.tsx
@@ -1,6 +1,6 @@
-// FollowButton.tsx
 import React from "react";
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 interface FollowButtonProps {
   isFollowing: boolean;
@@ -12,10 +12,29 @@ const FollowButton: React.FC<FollowButtonProps> = ({
   handleFollowToggle,
 }) => {
   return (
-    <TouchableOpacity onPress={handleFollowToggle} style={styles.button}>
-      <Text style={styles.buttonText}>
-        {isFollowing ? "Unfollow" : "Follow"}
-      </Text>
+    <TouchableOpacity
+      onPress={handleFollowToggle}
+      style={[
+        styles.button,
+        { backgroundColor: isFollowing ? "#5BADCE" : "white" },
+      ]}
+    >
+      <View style={styles.buttonContent}>
+        <Text
+          style={[
+            styles.buttonText,
+            { color: isFollowing ? "white" : "#5BADCE" },
+          ]}
+        >
+          {isFollowing ? "Unfollow" : "Follow"}
+        </Text>
+        <Icon
+          name={isFollowing ? "account-minus" : "account-plus"}
+          size={20}
+          color={isFollowing ? "white" : "#5BADCE"}
+          style={styles.icon}
+        />
+      </View>
     </TouchableOpacity>
   );
 };
@@ -24,12 +43,22 @@ const styles = StyleSheet.create({
   button: {
     paddingVertical: 10,
     paddingHorizontal: 20,
-    backgroundColor: "#5BADCE",
-    borderRadius: 5,
+    borderRadius: 30,
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 5,
+    borderWidth: 1,
+  },
+  buttonContent: {
+    flexDirection: "row",
+    alignItems: "center",
   },
   buttonText: {
-    color: "white",
     fontWeight: "bold",
+    marginRight: 8,
+  },
+  icon: {
+    marginLeft: 8,
   },
 });
 
