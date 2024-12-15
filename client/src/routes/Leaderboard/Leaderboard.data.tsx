4062c8554ff047578895b99e445e0add9dd67026
hks1444 <hasan-kerem2003@hotmail.com>
2024-12-15 18:21:31 +0300

    feat(client): fetch leaderboard from backend

--- client/src/routes/Leaderboard/Leaderboard.data.tsx
+++ client/src/routes/Leaderboard/Leaderboard.data.tsx
@@ -1,3 +1,26 @@
-export const leaderboardLoader = () => {
-    return { leaderboard: [] } as const;
-};
+import { LoaderFunction } from "react-router";
+import { safeParse } from "valibot";
+import apiClient from "../../api";
+import { logger } from "../../utils";
+import { leaderboardSchema } from "./Leaderboard.schema";
+
+export const leaderboardLoader = (async ({ request }) => {
+    try {
+        const response = await apiClient.get("leaderboard/");
+        // const response = await fetch(`${BASE_URL}/leaderboard/`);
+        // const data = await response.json();
+        const { output, issues, success } = safeParse(
+            leaderboardSchema,
+            response.data,
+        );
+
+        if (!success) {
+            throw new Error(`Failed to parse post response: ${issues}`);
+        }
+
+        return output;
+    } catch (error) {
+        logger.error(`Error fetching leaderboard`, error);
+        throw new Error(`Failed to load leaderboard`);
+    }
+}) satisfies LoaderFunction;
