53ace778c9b5549b3154536601873ae6d91f3cbd
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2024-12-15 20:12:10 +0300

    feat(mobile): refactor NewQuizQuestion to use global API URL configuration

--- mobile/app/components/NewQuizQuestion.tsx
+++ mobile/app/components/NewQuizQuestion.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import API_URL_GLOBAL from "../../config";
 interface NewQuizQuestionProps {}
 
-const API_URL = "http://138.68.97.90/api/v1";
+//const API_URL = "http://138.68.97.90/api/v1";
 // const API_URL = "http://10.0.2.2:8000/api/v1";
 
 const NewQuizQuestion: React.FC<NewQuizQuestionProps> = ({}) => {
   return (
-    <View style={styles.container}>
+    //<View style={styles.container}>
+    <View>
       <Text>New Quiz Question</Text>
     </View>
   );
