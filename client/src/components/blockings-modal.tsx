7a09120a650120c0dbdf9d60ddbff291c3d0f1f8
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-12-16 17:59:52 +0300

    feat(client): add following and follower count

--- client/src/components/blockings-modal.tsx
+++ client/src/components/blockings-modal.tsx
@@ -16,6 +16,7 @@ export const BlockingModal = ({ blockings }: { blockings: Blockings[] }) => {
     const blockedUsers = blockings.slice(0, displayCount);
     const hasMore = displayCount < blockings.length;
     const [open, setOpen] = useState(false);
+    if (!blockings.length) return null;
     return (
         <section aria-label="Blocked Users" className="flex flex-col gap-4">
             <Button
@@ -23,19 +24,21 @@ export const BlockingModal = ({ blockings }: { blockings: Blockings[] }) => {
                 className={buttonClass({ intent: "tertiary", size: "medium" })}
             >
                 <span className={buttonInnerRing({ intent: "tertiary" })} />
-                See Blocked Users
+                See blocked users ({blockedUsers.length})
             </Button>
             <Dialog
                 open={open}
                 onClose={() => setOpen(false)}
-                className="fixed inset-0 z-50 m-auto flex h-fit max-h-[calc(100dvh-var(--inset)*2)] flex-col gap-4 overflow-auto rounded-[0.75rem] bg-white p-4 text-black shadow-[0_25px_50px_-12px_rgb(0_0_0_/_0.25)] sm:bottom-[10vh] sm:top-[10vh] sm:mt-0 sm:max-h-[80vh] sm:w-[320px] sm:rounded-[1rem] sm:p-6"
+                className="fixed inset-0 z-50 m-auto flex h-fit max-h-[calc(100dvh-var(--inset)*2)] flex-col gap-4 overflow-auto rounded-1 bg-white p-4 text-slate-800 shadow-xl sm:bottom-[10vh] sm:top-[10vh] sm:mt-0 sm:max-h-[80vh] sm:w-[480px] sm:rounded-[1rem] sm:p-6"
             >
-                <DialogHeading className="heading">Blocked Users</DialogHeading>
+                <DialogHeading className="text-xs font-medium uppercase text-slate-500">
+                    Blocked Users
+                </DialogHeading>
                 <div className="flex w-full flex-col items-center gap-4">
                     {blockedUsers.length > 0
                         ? blockedUsers.map((user) => (
-                              <div className="flex w-full flex-row justify-between">
-                                  <div className="flex flex-row gap-2">
+                              <div className="flex w-full items-center gap-4 rounded-1 bg-white p-4 ring ring-slate-200 transition-colors hover:bg-slate-100">
+                                  <div className="flex flex-1 flex-row items-center gap-4">
                                       <Avatar
                                           author={{
                                               full_name: user.full_name,
@@ -44,11 +47,11 @@ export const BlockingModal = ({ blockings }: { blockings: Blockings[] }) => {
                                           }}
                                           size={40}
                                       />
-                                      <div className="flex flex-col items-start">
-                                          <p className="text-sm font-medium text-slate-900">
+                                      <div className="flex w-full max-w-48 flex-col items-start">
+                                          <span className="font-medium text-slate-900">
                                               {user.full_name}
-                                          </p>
-                                          <p className="text-sm text-slate-700">
+                                          </span>
+                                          <p className="text-sm text-slate-500">
                                               @{user.username}
                                           </p>
                                       </div>
