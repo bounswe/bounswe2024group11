b5f5cc52da6d5dd56e88740b51cb4d7b90ed9a05
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-12-16 19:22:57 +0300

    fix(client): display proficiency change only on own profile

--- client/src/routes/Profile/Profile.tsx
+++ client/src/routes/Profile/Profile.tsx
@@ -121,7 +121,7 @@ export const Profile = () => {
                                             </p>
                                         </div>
                                     </div>
-                                    {!isMe ? (
+                                    {isMe ? (
                                         <>
                                             {!is_blocked ? (
                                                 <blockFetcher.Form
@@ -286,12 +286,14 @@ export const Profile = () => {
                                         )}
                                     </span>
                                 </div>
-                                <div>
-                                    <ProficiencyChange
-                                        proficiency={proficiency}
-                                        username={username}
-                                    />
-                                </div>
+                                {isMe && (
+                                    <div>
+                                        <ProficiencyChange
+                                            proficiency={proficiency}
+                                            username={username}
+                                        />
+                                    </div>
+                                )}
                             </header>
                             <Ariakit.Separator className="ring-slate-200" />
 
