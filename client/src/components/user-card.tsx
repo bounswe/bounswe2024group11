41c5932688511759f0c2e4bae086da2daa3457b9
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-12-16 17:27:58 +0300

    add suggested people into the feed

--- client/src/components/user-card.tsx
+++ client/src/components/user-card.tsx
@@ -10,11 +10,11 @@ export const UserCard = ({ user }: { user: Author }) => {
     const followFetcher = useFetcher<typeof FollowAction>();
     const [isFollowDisabled, setIsFollowDisabled] = useState(false);
     return (
-        <div className="flex gap-4 rounded-1 bg-white p-4 ring ring-slate-200">
-            <Link
-                to={`/profile/${user.username}`}
-                className="flex flex-row items-center gap-4"
-            >
+        <Link
+            to={`/profile/${user.username}`}
+            className="flex items-center gap-4 rounded-1 bg-white p-4 ring ring-slate-200 transition-colors hover:bg-slate-100"
+        >
+            <div className="flex flex-1 flex-row items-center gap-4">
                 <Avatar author={user} size={48} />
                 <div className="flex w-full max-w-48 flex-col items-start">
                     <span className="font-medium text-slate-900">
@@ -22,30 +22,54 @@ export const UserCard = ({ user }: { user: Author }) => {
                     </span>
                     <p className="text-sm text-slate-500">@{user.username}</p>
                 </div>
-            </Link>
-            <followFetcher.Form
-                method="POST"
-                action={`follow/`}
-                onSubmit={() => setIsFollowDisabled(true)}
-            >
+            </div>
+            {!user.is_followed && (
+                <followFetcher.Form
+                    method="POST"
+                    action={`profile/${user.username}/follow/`}
+                    onSubmit={() => setIsFollowDisabled(true)}
+                >
+                    <Button
+                        className={buttonClass({
+                            intent: "secondary",
+                            size: "medium",
+                            className: "w-20",
+                        })}
+                        type="submit"
+                        disabled={isFollowDisabled}
+                    >
+                        <span
+                            className={buttonInnerRing({
+                                intent: "secondary",
+                            })}
+                        />
+                        <span>Follow</span>
+                    </Button>
+                    <input
+                        type="hidden"
+                        name="following"
+                        value={user.id}
+                    ></input>
+                </followFetcher.Form>
+            )}
+            {user.is_followed && (
                 <Button
                     className={buttonClass({
                         intent: "secondary",
                         size: "medium",
-                        className: "w-20",
+                        className: "",
                     })}
                     type="submit"
-                    disabled={isFollowDisabled}
+                    disabled={true}
                 >
                     <span
                         className={buttonInnerRing({
                             intent: "secondary",
                         })}
                     />
-                    <span>Follow</span>
+                    <span>Following</span>
                 </Button>
-                <input type="hidden" name="following" value={user.id}></input>
-            </followFetcher.Form>
-        </div>
+            )}
+        </Link>
     );
 };
--- client/src/routes/Home/HomePeopleSuggestions.tsx
+++ client/src/routes/Home/HomePeopleSuggestions.tsx
@@ -5,9 +5,9 @@ export const HomePeopleSuggestions = ({ people }: { people: Author[] }) => {
     return (
         <section className="flex flex-col gap-2">
             <h2 className="text-sm leading-6 text-slate-500">
-                We believe you might be interested in
+                These people stand out to you
             </h2>
-            <div className="flex gap-2">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                 {people.map((author) => {
                     if (author.is_followed === 1) return null;
                     return <UserCard user={author} key={author.id} />;
