2c984dcf4c08c6a7cdbda4d9a3dc9e4b51b4b527
Muhammet Emin Çiftçi <115792721+meminciftci@users.noreply.github.com>
2024-12-16 03:01:46 +0300

    feat(be): implement sorting for forum page and quiz page  (#864)
    * feat(be): implement sorting for forum questions
    
    * update(be): implement sorting for quizzes
    
    * remove redundant comments
    
    ---------
    
    Co-authored-by: Ümit Can Evleksiz <evleksizumitcan@gmail.com>
    Co-authored-by: Ümit Can Evleksiz <68506701+umitcan07@users.noreply.github.com>

--- backend/core/views/forum_views.py
+++ backend/core/views/forum_views.py
@@ -11,6 +11,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from ..utils import get_ids
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from django.db.models import Count
 
 class ForumQuestionPagination(PageNumberPagination):
     page_size = 10
@@ -26,10 +27,23 @@ class ForumQuestionViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
+        sort_by = self.request.query_params.get('sort_by', 'oldest')
+
         if user.is_authenticated:
             blocked_users = Block.objects.filter(blocker=user).values_list('blocking', flat=True)
-            return ForumQuestion.objects.exclude(author__in=blocked_users).order_by('-created_at')
-        return ForumQuestion.objects.all().order_by('-created_at')
+            queryset = ForumQuestion.objects.exclude(author__in=blocked_users)
+        else:
+            queryset = ForumQuestion.objects.all()
+
+        if sort_by == 'newest':
+            return queryset.order_by('-created_at')
+        elif sort_by == 'oldest':
+            return queryset.order_by('created_at')
+        elif sort_by == 'most_popular':
+            return queryset.annotate(answers_count=Count('answers')).order_by('-answers_count', '-created_at')
+        elif sort_by == 'most_liked':
+            return queryset.order_by('upvotes', '-created_at')
+        
     
     def get_parsers(self):
         if getattr(self, 'swagger_fake_view', False):
@@ -49,6 +63,13 @@ class ForumQuestionViewSet(viewsets.ModelViewSet):
                 type=openapi.TYPE_STRING,
                 description="ID for linked data semantic search"
             ),
+            openapi.Parameter(
+                'sort_by',
+                openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                enum=['newest', 'oldest', 'most_popular', 'most_liked'],
+                description="Sorting criteria for forum questions",
+            ),
         ]
     )
     def list(self, request, *args, **kwargs):
@@ -58,17 +79,15 @@ class ForumQuestionViewSet(viewsets.ModelViewSet):
         linked_data_id = self.request.query_params.get('linked_data_id')
         if linked_data_id:
             linked_data_ids = get_ids(linked_data_id)
-            queryset = self.filter_queryset(self.get_queryset()).filter(tags__linked_data_id__in=linked_data_ids).order_by('-created_at')
+            queryset = self.filter_queryset(self.get_queryset()).filter(tags__linked_data_id__in=linked_data_ids)
         else:
-            queryset = self.filter_queryset(self.get_queryset()).order_by('-created_at')
+            queryset = self.filter_queryset(self.get_queryset())
 
-        # Apply pagination
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
-        # Fallback: If pagination is not applied, return the full dataset
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
@@ -78,7 +97,7 @@ class ForumQuestionViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def get_permissions(self):
-        if self.action == 'list':  # If listing, allow anyone
+        if self.action == 'list':
             return [permissions.AllowAny()]
         return super().get_permissions()
     
@@ -91,16 +110,14 @@ class ForumAnswerViewSet(viewsets.ModelViewSet):
         user = self.request.user
         if user.is_authenticated:
             blocked_users = Block.objects.filter(blocker=user).values_list('blocking', flat=True)
-            # Filter answers by the question ID and blocked users
             return ForumAnswer.objects.filter(forum_question_id=self.kwargs['forum_question_pk']).exclude(author__in=blocked_users).order_by('created_at')
         return ForumAnswer.objects.filter(forum_question_id=self.kwargs['forum_question_pk']).order_by('created_at')
     
     def perform_create(self, serializer):
-        # Set the author to the current authenticated user
         serializer.save(author=self.request.user, forum_question_id=self.kwargs['forum_question_pk'])
    
     def get_permissions(self):
-        if self.action == 'list':  # If listing, allow anyone
+        if self.action == 'list': 
             return [permissions.AllowAny()]
         return super().get_permissions()
 
